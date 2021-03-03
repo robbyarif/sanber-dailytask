c86e613e74057efa7da3f90595e79bb35a35b48c
Robby Arifandri <robby.arifandri@gmail.com>
2021-03-03 22:00:21 +0700

    Add route and pages extending from template

--- week3/d2-laravel/SanberBook/routes/web.php
+++ week3/d2-laravel/SanberBook/routes/web.php
@@ -11,9 +11,19 @@
 |
 */
 
-Route::get('/', 'HomeController@get')->name('index');
+// Route::get('/', 'HomeController@get')->name('index');
 
 Route::get('/register', 'AuthController@get_register');
 Route::post('/register', 'AuthController@post_register');
 
 Route::get('/welcome', 'AuthController@get_welcome')->name('welcome');
+
+// TODO set this path as root
+Route::get('/', function () {
+    return view('items.index');
+});
+
+Route::get('/data-tables', function (){
+    return view('items.details');
+});
+
